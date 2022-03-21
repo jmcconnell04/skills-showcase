a0ab95d9402decf4ca5f1bd0d1d2d2d4e34fb6c7
James McConnell <jmcconnell04@verizon.net>
2022-03-21 19:55:54 -0400

    Grid, Cells, Tile

---
+++ html/2048-game-demo/js/script.js
@@ -0,0 +1,8 @@
+import Grid from './grid.js';
+import Tile from './tile.js';
+
+const gameBoard = document.getElementById('game-board');
+const grid = new Grid(gameBoard);
+
+grid.randomEmptyCell().title = new Tile(gameBoard);
+grid.randomEmptyCell().title = new Tile(gameBoard);
